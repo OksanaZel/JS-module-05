bc30070d4e98dae4e022c5d1fd5cf97f310d57de
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-03-10 19:32:30 +0200

    Добавляет задачи

--- js/05module-hw.js
+++ js/05module-hw.js
@@ -473,4 +473,81 @@ price и приватного статического свойства MAX_PRIC
 // console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
 // console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.
 
-/*17. */
+/*17. В приложении нужен администратор с возможностью добавлять почты 
+пользователей в чёрный список.
+Объяви класс Admin, который наследует от класса User.
+Добавь классу Admin публичное статическое свойство AccessLevel 
+(уровень доступа), значение которого это объект { BASIC: 'basic', 
+SUPERUSER: 'superuser' }. */
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//     static AccessLevel = {
+//         BASIC: 'basic',
+//         SUPERUSER: 'superuser',
+//     }
+// }
+
+// console.log(Admin.AccessLevel.BASIC);
+
+/*18. Добавь классу Admin метод constructor, который принимает 
+один параметр - объект настроек с двумя свойствами email и accessLevel. 
+Добавь классу Admin публичное свойсво accessLevel, значение которого 
+будет передаваться при вызове конструктора.
+Чтобы показать как будет использоваться класс Admin, мы добавили 
+инициализацию экземпляра под объявлением класса. */
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class Admin extends User {
+  // Пиши код ниже этой строки
+     constructor({email, accessLevel}) {
+         super(email);
+         this._accessLevel = accessLevel;
+    }
+
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser'
+    };
+   
+
+  // Пиши код выше этой строки
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango._accessLevel); // superuser
